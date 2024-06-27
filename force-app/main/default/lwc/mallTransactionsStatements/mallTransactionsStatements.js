6dfe3f8181e4703a034ed0efaa55e860c60f2566
aneelaoad <aneelaoadd@gmail.com>
2024-06-27 15:40:16 +0500

    download and view buttons working 100%

--- force-app/main/default/lwc/mallTransactionsStatements/mallTransactionsStatements.js
+++ force-app/main/default/lwc/mallTransactionsStatements/mallTransactionsStatements.js
@@ -8,6 +8,7 @@ import sbgIcons from '@salesforce/resourceUrl/sbgIcons';
 import getStatements from '@salesforce/apex/MallStatementsCtrl.getStatements';
 import getCustomerDocumentByUUID from "@salesforce/apex/CTRL_MallDocumentManagement.getCustomerDocumentByUUID";
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getCustomerDocumentForViewing from "@salesforce/apex/CTRL_MallDocumentManagement.getCustomerDocumentForViewing";
 
 /**
  * @class MallTransactionsStatements
@@ -59,22 +60,37 @@ export default class MallTransactionsStatements extends LightningElement {
    * @param {Event} event - The event object from the button click.
    */
   async handleUnstampedDocumentDownload(event) {
-    const documentUUID = event.currentTarget.dataset.documentuuid;
-    const selectedBtn = event.currentTarget;
-    console.log(documentUUID);
-    console.log(selectedBtn);
-    try {
-        this.showSpinner = true;
-        const link = await getCustomerDocumentByUUID({ documentUUID });
-        this.showSpinner = false;
-        window.open(link, "_blank");
-    } catch (error) {
-        this.showSpinner = false;
-        this.error = error;
-        console.error('Error downloading document: ', error);
-    } finally {
-        this.closeContextMenu(documentUUID);
+    const downloadUrl = event.currentTarget.dataset.downloadurl;
+    if (downloadUrl) {
+      window.open(downloadUrl, "_blank");
+
+    } else  {
+      const documentUUID = event.currentTarget.dataset.documentuuid;
+      const selectedBtn = event.currentTarget;
+      console.log(documentUUID);
+      console.log(selectedBtn);
+      try {
+          this.showSpinner = true;
+          const link = await getCustomerDocumentByUUID({ documentUUID });
+          this.showSpinner = false;
+          this.statements.forEach(statement => {
+            
+            if (statement.uid === documentUUID) {
+              statement.downloadUrl = link.downloadUrl;
+              statement.publicUrl = link.publicViewUrl;
+            }
+          });
+          window.open(link.downloadUrl, "_blank");
+      } catch (error) {
+          this.showSpinner = false;
+          this.error = error;
+          console.error('Error downloading document: ', error);
+      } finally {
+          this.closeContextMenu(documentUUID);
+      }
+  
     }
+    // const publicUrl = event.currentTarget.dataset.publicUrl
 }
  
     /**
@@ -83,21 +99,37 @@ export default class MallTransactionsStatements extends LightningElement {
      * @param {Event} event - The event object from the statement title click.
      */
     async viewStatement(event) {
-      const documentUUID = event.currentTarget.dataset.documentuuid;
-      console.log('View statement:', documentUUID);
+      const publicUrl = event.currentTarget.dataset.publicurl;
+    if (publicUrl) {
+      window.open(publicUrl, "_blank");
 
+    } else  {
+      const documentUUID = event.currentTarget.dataset.documentuuid;
+      const selectedBtn = event.currentTarget;
+      console.log(documentUUID);
+      console.log(selectedBtn);
       try {
           this.showSpinner = true;
           const link = await getCustomerDocumentByUUID({ documentUUID });
           this.showSpinner = false;
-          window.open(link, "_blank");
+          this.statements.forEach(statement => {
+            
+            if (statement.uid === documentUUID) {
+              statement.downloadUrl = link.downloadUrl;
+              statement.publicUrl = link.publicViewUrl;
+            }
+          });
+          window.open(link.publicViewUrl, "_blank");
       } catch (error) {
           this.showSpinner = false;
           this.error = error;
-          console.error('Error viewing document: ', error);
+          console.error('Error downloading document: ', error);
+      } finally {
+          this.closeContextMenu(documentUUID);
       }
+  
+    }
   }
-
 
   /**
    * @method deleteStatement
@@ -113,28 +145,4 @@ export default class MallTransactionsStatements extends LightningElement {
 
 
 
-
-  /**
-   * @method showContextMenu
-   * @description Toggles the context menu ie. view, delelet and download for a statement.
-   * @param {Event} event - The event object from the button click.
-   */
-  showContextMenu(event) {
-    const documentUUID = event.currentTarget.dataset.documentuuid;
-    this.statements = this.statements.map(statement => ({
-      ...statement,
-      showMenu: statement.uid === documentUUID ? !statement.showMenu : false
-    }));
-  }
-   /**
-     * @method closeContextMenu
-     * @description Closes the context menu for the statement with the specified UUID.
-     * @param {String} documentUUID - The UUID of the statement to close the context menu for.
-     */
-   closeContextMenu(documentUUID) {
-    this.statements = this.statements.map(statement => ({
-        ...statement,
-        showMenu: statement.uid === documentUUID ? false : statement.showMenu
-    }));
-}
 }
