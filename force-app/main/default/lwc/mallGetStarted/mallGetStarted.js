ca5782aec789242a4952dabcc372737beea3367c
mahamhassan <aneelaoadd@gmail.com>
2024-07-22 14:30:10 +0500

    updated 22nd july

--- force-app/main/default/lwc/mallGetStarted/mallGetStarted.js
+++ force-app/main/default/lwc/mallGetStarted/mallGetStarted.js
@@ -1,13 +1,15 @@
-import { LightningElement ,wire} from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import Resources from "@salesforce/resourceUrl/sbgVisualAssets";
 import getSolutionLinks from '@salesforce/apex/CTRL_MallSolutions.getSolutionLinks';
-import getOfferings from '@salesforce/apex/OfferingsSelector.getContentUnitTranslationsForOfferingsByCategory';
+// import getOfferings from '@salesforce/apex/OfferingsSelector.getContentUnitTranslationsForOfferingsByCategory';
+import getTagOfferingsByCategoryNames from "@salesforce/apex/MallDataService.getTagOfferingsByCategoryNames";
+
 
 export default class MallGetStarted extends LightningElement {
-  
-    offeringsData = []
+
 
     solutionsData = []
+    offeringsList = []
 
     @wire(getSolutionLinks)
     wiredSolutionLinks({ error, data }) {
@@ -25,14 +27,27 @@ export default class MallGetStarted extends LightningElement {
         }
     }
 
-    // @wire(getOfferings)
-    // wriedOfferings({ error, data }) {
-    //     if (data) {
-    //     this.offeringsData = data
-    //      console.log('offeringsData: ', JSON.stringify(this.offeringsData));
 
-    //     } else if (error) {
-    //         console.error('error fetching solution:',error);
-    //     }
-    // }
+
+
+ // ------------------------------
+    connectedCallback() {
+
+
+        getTagOfferingsByCategoryNames()
+            .then(result => {
+                this.offeringsList = result;
+                console.log('Tag Offerings:', this.offeringsList);
+                this.offeringsList.forEach(element => {
+                    console.log('offering: ', element.name);
+                });
+
+            })
+            .catch(error => {
+                console.error('Error fetching tag offerings:', error);
+                // Handle error
+            });
+    }
+
+
 }
